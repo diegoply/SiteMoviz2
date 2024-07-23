ce45757445f0580bb79c21a19bcf56821b2b5166
Diego Ply <ply.dgo@gmail.com>
2024-07-23 11:18:59 +0200

    first commit

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+import 'bootstrap.esm.min.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import 'bootstrap';
+import '@popperjs/core';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/app.css'
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
