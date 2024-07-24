19f5450129a7ea30ddb9581f44cbcb476b51e5d0
Diego Ply <ply.dgo@gmail.com>
2024-07-24 09:51:16 +0200

    easyadmin en cours

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Director;
+use App\Entity\Genre;
+use App\Entity\Movie;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -41,6 +44,9 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Genre', 'fas fa-list', Genre::class);
+        yield MenuItem::linkToCrud('Réalisateurs', 'fa-solid fa-clapperboard', Director::class);
+        yield MenuItem::linkToCrud('Film', 'fas fa-film', Movie::class);
+        
     }
 }
---
+++ src/Controller/Admin/MovieCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Movie;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class MovieCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Movie::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+{
+    yield TextField::new('name', "Nom");
+    yield DateTimeField::new('release_date', "Date de sortie");
+    yield AssociationField::new('directors', "Réalisateur");
+    yield AssociationField::new('genres', "Genre");
+}
+}
