1ddb41aee94a5a91609971e664a4386238ac1679
Diego Ply <ply.dgo@gmail.com>
2024-07-24 18:53:01 +0200

    FrontDebut Reviews

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/films', name: 'app_movie')]
+    #[Route('/films', name: 'app_movies')]
     public function index(MovieRepository $movieRepository): Response
     {
         $movies =  $movieRepository->findBy([], ['id' => 'DESC']);
@@ -18,6 +19,16 @@ class MovieController extends AbstractController
 
         return $this->render('movie/index.html.twig', [
             'movies' => $movies,
+        ]);
+    }
+
+    #[Route('/films/{id}', name: 'app_show_movie')]
+    public function show(Movie $movie): Response
+    {
+    
+
+        return $this->render('movie/show.html.twig', [
+            'movie' => $movie,
         ]);
     }
 }
